521826562741e23f216e1da83a5aef2b69019739
RCC1783 <75766592+RCC1783@users.noreply.github.com>
2025-03-14 00:14:06 -0700

    Made sure everything was working

--- IAT267_Proj_arduino_code/IAT267_Proj_arduino_code.ino
+++ IAT267_Proj_arduino_code/IAT267_Proj_arduino_code.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 //#include <SparkFun_TB6612.h>
 
-enum analogInPins {ROT_SENSOR_IN = A0, FWD_BCK_SLIDER_IN = A1, WGHT_SENSOR_IN = A2, CLR_SENSOR_IN = A3};
+enum analogInPins {ROT_SENSOR_IN = 0, FWD_BCK_SLIDER_IN = 1, WGHT_SENSOR_IN = 2, CLR_SENSOR_IN = 3};
 enum analogOutPins {ROD_ROTATOR_OUT = 11, ROD_MOVER_OUT = 10};
 
 
@@ -28,13 +28,14 @@ const int GAME_TITLE = 0;
 const int PLAYMODE = 1;
 const int GAME_END = 2;
 
-unsigned long gameState = 0;
-unsigned long gameTimeMax = 10000;
+unsigned int gameState = 0;
 
+unsigned long gameTimeMax = 60000; //1 minute
 unsigned long gameTime = 0;
 
 void setup() {
   Serial.begin(9600);
+  while(!Serial){;}
 
   rodRotator.attach(ROD_ROTATOR_OUT);
   rodFwdBk.attach(ROD_MOVER_OUT);
@@ -43,81 +44,89 @@ void setup() {
   pinMode(REEL_MOTOR_AI1, OUTPUT);
   pinMode(REEL_MOTOR_AI2, OUTPUT);
   pinMode(REEL_MOTOR_STBY, OUTPUT);
+
+  rodRotator.write(90);
+  rodFwdBk.write(90);
 }
 
 void loop() {
-
-  switch(gameState){
-    case(GAME_TITLE):
-      char keystroke;
-      if(Serial.available() > 0){
-        keystroke = Serial.read();
-
-        if(keystroke == " "){
+  switch (gameState){
+    case GAME_TITLE:
+      {
+        byte keystroke;
+        if(Serial.available() > 0){
           gameState = PLAYMODE;
           gameTime = millis();
         }
       }
       break;
-    case(PLAYMODE):
-      if(Serial.available() > 0){
-         Serial.write(millis() - gameTime); //sends game time to processing 
-      }
-      
-      if(gameTime + gameTimeMax >= millis()){
-        gameState = GAME_END;
-        if(lineReelDown == true){
-          reelTime = 0;
+    case PLAYMODE:
+      {
+        Serial.print("time remaining: ");
+        Serial.println(gameTime + gameTimeMax - millis());
+        if(millis() >= gameTime + gameTimeMax){
+//          Serial.println("Game Over trigger");
+          gameState = GAME_END;
+  //        if(lineReelDown == true){
+  //          reelTime = 0;
+  //        }
         }
-        Serial.write(gameState);
-        return;
-      }
-    
-      int force = analogRead(WGHT_SENSOR_IN);
-
-      if(Serial.available() > 0){
-        Serial.write(force); //sends the force to processing to be converted to weight
-      }
-
-      rodRotator.write(AnalogInToDegrees180(ROT_SENSOR_IN));
-      rodFwdBk.write(AnalogInToDegrees180(FWD_BCK_SLIDER_IN));
-    
-      ReelController();
+        int force = analogRead(WGHT_SENSOR_IN);
+//        Serial.write(force);
+        
+        rodRotator.write(AnalogInToDegrees180(ROT_SENSOR_IN));
+        rodFwdBk.write(AnalogInToDegrees180(FWD_BCK_SLIDER_IN));
   
-      delay(15);
-      break;
-    case GAME_END: // Should Reset everything?
-      // raise the line back up
-      if(lineReelDown == false && reelTime < REEL_TIME_MAX){
-        Serial.println("reelingUp");
-        reelTime++;
-        
-        //Spin Counter-Clockwise at speed 150
-        digitalWrite(REEL_MOTOR_STBY, HIGH);
-        analogWrite(REEL_MOTOR_PWMA, 150);
-        digitalWrite(REEL_MOTOR_AI1, LOW); 
-        digitalWrite(REEL_MOTOR_AI2, HIGH); 
-        if(reelTime >= REEL_TIME_MAX){
-          Serial.println("switch to true");
-          lineReelDown == true;
-        }else{
-          digitalWrite(REEL_MOTOR_STBY, LOW);
-          analogWrite(REEL_MOTOR_PWMA, 0);
-        }
+        ReelController();
+  
+        delay(15);
       }
-      
-      rodRotator.write(90);
-      rodFwdBk.write(90);
-        
+      break;
+    case GAME_END:
+      {
+        Serial.println("end");
+        // raise the line back up
+        if(lineReelDown == false && reelTime < REEL_TIME_MAX){
+//          Serial.println("reelingUp");
+          reelTime++;
+          
+          //Spin Counter-Clockwise at speed 150
+          digitalWrite(REEL_MOTOR_STBY, HIGH);
+          analogWrite(REEL_MOTOR_PWMA, 150);
+          digitalWrite(REEL_MOTOR_AI1, LOW); 
+          digitalWrite(REEL_MOTOR_AI2, HIGH); 
+          if(reelTime >= REEL_TIME_MAX){
+//            Serial.println("switch to true");
+            lineReelDown == true;
+          }else{
+            digitalWrite(REEL_MOTOR_STBY, LOW);
+            analogWrite(REEL_MOTOR_PWMA, 0);
+          }
+        }
+
+        rodRotator.write(90);
+        rodFwdBk.write(90);
+
+//        byte keystroke;
+//        if(Serial.available() > 0){
+//          gameState = PLAYMODE;
+//          gameTime = millis();
+//        }
+      }
       break;
     default:
-      break;
+      {
+        Serial.println("default");
+        delay(10);
+      }
   }
+  delay(15);
 }
 
 int AnalogInToDegrees180(int analogIn){
   int rotVal = analogRead(analogIn);
   rotVal = map(rotVal, 0, 1023, 0, 180);
+  
   return rotVal;
 }
 
@@ -125,10 +134,8 @@ void ReelController(){
   if(digitalRead(REEL_BUTTON_PIN) == HIGH && buttonInputTimer == 0){
     if(lineReelDown == true){
       lineReelDown = false;
-      Serial.println("reel up");
     }else if(lineReelDown == false){
       lineReelDown = true;
-      Serial.println("reel down");
     }
     buttonInputTimer = BUTTON_INPUT_TIMER_DELAY_MAX;
     reelTime = 0;
@@ -137,7 +144,7 @@ void ReelController(){
   if(buttonInputTimer > 0){buttonInputTimer -= 1;}
 
   if(lineReelDown == true && reelTime < REEL_TIME_MAX){
-    Serial.println("reelingDown");
+//    Serial.println("reelingDown");
     reelTime++;
     
     //Spin Clockwise at speed 150
@@ -146,12 +153,12 @@ void ReelController(){
     digitalWrite(REEL_MOTOR_AI1, HIGH); 
     digitalWrite(REEL_MOTOR_AI2, LOW); 
     if(reelTime >= REEL_TIME_MAX){
-      Serial.println("switch to false");
+//      Serial.println("switch to false");
       lineReelDown == false;
     }
   }
   else if(lineReelDown == false && reelTime < REEL_TIME_MAX){
-    Serial.println("reelingUp");
+//    Serial.println("reelingUp");
     reelTime++;
     
     //Spin Counter-Clockwise at speed 150
@@ -160,7 +167,7 @@ void ReelController(){
     digitalWrite(REEL_MOTOR_AI1, LOW); 
     digitalWrite(REEL_MOTOR_AI2, HIGH); 
     if(reelTime >= REEL_TIME_MAX){
-      Serial.println("switch to true");
+//      Serial.println("switch to true");
       lineReelDown == true;
     }
   }else{
