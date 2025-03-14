3c3f3ba39b3089a78a6b0ac383b83e73bf07e943
RCC1783 <75766592+RCC1783@users.noreply.github.com>
2025-03-13 18:18:18 -0700

    Game states and some other stuff

--- IAT267_Proj_arduino_code/IAT267_Proj_arduino_code.ino
+++ IAT267_Proj_arduino_code/IAT267_Proj_arduino_code.ino
@@ -1,19 +1,21 @@
 #include <Servo.h>
 //#include <SparkFun_TB6612.h>
 
-enum analogSensorPins {ROT_SENSOR_PIN, FWD_BCK_SLIDER_PIN, WHGHT_SENSOR_PIN};
+enum analogInPins {ROT_SENSOR_IN = A0, FWD_BCK_SLIDER_IN = A1, WGHT_SENSOR_IN = A2, CLR_SENSOR_IN = A3};
+enum analogOutPins {ROD_ROTATOR_OUT = 11, ROD_MOVER_OUT = 10};
+
 
 //Digital 
-const int REEL_BUTTON_PIN = 2;
+const int REEL_BUTTON_PIN = 2; //reel in/out button (digital)
 
 //Pins for the DC motor
-const int REEL_MOTOR_AI1 = 4; //direction control 1
-const int REEL_MOTOR_AI2 = 7; //direction control 2
-const int REEL_MOTOR_STBY = 8; //standby mode control
-const int REEL_MOTOR_PWMA = 3; //speed of motor
+const int REEL_MOTOR_AI1 = 4; //direction control 1 (digital)
+const int REEL_MOTOR_AI2 = 7; //direction control 2 (digital)
+const int REEL_MOTOR_STBY = 8; //standby mode control (digital)
+const int REEL_MOTOR_PWMA = 3; //speed of motor (analog)
 
-Servo armRotator;
-Servo armFwdBk;
+Servo rodRotator;
+Servo rodFwdBk;
 
 bool lineReelDown = true;
 int reelTime = 0;
@@ -22,22 +24,104 @@ const int REEL_TIME_MAX = 50;
 int buttonInputTimer = 0;
 const int BUTTON_INPUT_TIMER_DELAY_MAX = 100;
 
+const int GAME_TITLE = 0;
+const int PLAYMODE = 1;
+const int GAME_END = 2;
+
+unsigned long gameState = 0;
+unsigned long gameTimeMax = 10000;
+
+unsigned long gameTime = 0;
+
 void setup() {
   Serial.begin(9600);
 
+  rodRotator.attach(ROD_ROTATOR_OUT);
+  rodFwdBk.attach(ROD_MOVER_OUT);
+
   pinMode(REEL_BUTTON_PIN, INPUT);
-  
   pinMode(REEL_MOTOR_AI1, OUTPUT);
   pinMode(REEL_MOTOR_AI2, OUTPUT);
   pinMode(REEL_MOTOR_STBY, OUTPUT);
 }
 
 void loop() {
-//  Serial.println(ROT_SENSOR_PIN);
-  
-  armRotator.write(AnalogInToDegrees180(ROT_SENSOR_PIN));
-  armFwdBk.write(AnalogInToDegrees180(FWD_BCK_SLIDER_PIN));
 
+  switch(gameState){
+    case(GAME_TITLE):
+      char keystroke;
+      if(Serial.available() > 0){
+        keystroke = Serial.read();
+
+        if(keystroke == " "){
+          gameState = PLAYMODE;
+          gameTime = millis();
+        }
+      }
+      break;
+    case(PLAYMODE):
+      if(Serial.available() > 0){
+         Serial.write(millis() - gameTime); //sends game time to processing 
+      }
+      
+      if(gameTime + gameTimeMax >= millis()){
+        gameState = GAME_END;
+        if(lineReelDown == true){
+          reelTime = 0;
+        }
+        Serial.write(gameState);
+        return;
+      }
+    
+      int force = analogRead(WGHT_SENSOR_IN);
+
+      if(Serial.available() > 0){
+        Serial.write(force); //sends the force to processing to be converted to weight
+      }
+
+      rodRotator.write(AnalogInToDegrees180(ROT_SENSOR_IN));
+      rodFwdBk.write(AnalogInToDegrees180(FWD_BCK_SLIDER_IN));
+    
+      ReelController();
+  
+      delay(15);
+      break;
+    case GAME_END: // Should Reset everything?
+      // raise the line back up
+      if(lineReelDown == false && reelTime < REEL_TIME_MAX){
+        Serial.println("reelingUp");
+        reelTime++;
+        
+        //Spin Counter-Clockwise at speed 150
+        digitalWrite(REEL_MOTOR_STBY, HIGH);
+        analogWrite(REEL_MOTOR_PWMA, 150);
+        digitalWrite(REEL_MOTOR_AI1, LOW); 
+        digitalWrite(REEL_MOTOR_AI2, HIGH); 
+        if(reelTime >= REEL_TIME_MAX){
+          Serial.println("switch to true");
+          lineReelDown == true;
+        }else{
+          digitalWrite(REEL_MOTOR_STBY, LOW);
+          analogWrite(REEL_MOTOR_PWMA, 0);
+        }
+      }
+      
+      rodRotator.write(90);
+      rodFwdBk.write(90);
+        
+      break;
+    default:
+      break;
+  }
+}
+
+int AnalogInToDegrees180(int analogIn){
+  int rotVal = analogRead(analogIn);
+  rotVal = map(rotVal, 0, 1023, 0, 180);
+  return rotVal;
+}
+
+void ReelController(){
   if(digitalRead(REEL_BUTTON_PIN) == HIGH && buttonInputTimer == 0){
     if(lineReelDown == true){
       lineReelDown = false;
@@ -83,12 +167,4 @@ void loop() {
     digitalWrite(REEL_MOTOR_STBY, LOW);
     analogWrite(REEL_MOTOR_PWMA, 0);
   }
-  
-  delay(15);
-}
-
-int AnalogInToDegrees180(int analogIn){
-  int rotVal = analogRead(analogIn);
-  rotVal = map(rotVal, 0, 1023, 0, 180);
-  return rotVal;
 }
